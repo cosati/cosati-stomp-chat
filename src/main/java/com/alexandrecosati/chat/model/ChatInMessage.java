67df5b883c5b4fa89ba57f57a47cebf417a53a99
Alexandre Cosati <alexandre_cosati@epam.com>
2022-04-25 11:28:45 +0200

    first commit - model

---
+++ src/main/java/com/alexandrecosati/chat/model/ChatInMessage.java
@@ -0,0 +1,42 @@
+package com.alexandrecosati.chat.model;
+
+import java.util.Date;
+
+public class ChatInMessage {
+    private String senderId;
+    private String senderName;
+    private String message;
+    private Date timeStamp;
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Date timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+}
---
+++ src/main/java/com/alexandrecosati/chat/model/ChatOutMessage.java
@@ -0,0 +1,33 @@
+package com.alexandrecosati.chat.model;
+
+import java.util.Date;
+
+public class ChatOutMessage {
+    private String content;
+    private String groupName;
+    private Date timeStamp;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Date timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+}
